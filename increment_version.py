fd478c09677451860f8311fa9b5f5e18f2dfe9c4
stevennolte <138683349+stevennolte@users.noreply.github.com>
2025-07-11 20:32:10 -0500

    Update increment_version.py

--- increment_version.py
+++ increment_version.py
@@ -3,13 +3,10 @@ import json
 with open("firmware.json", "r") as f:
     data = json.load(f)
 
-# Assume version is in the format "major.minor.patch"
-version = data.get("version", "0.0.0")
-major, minor, patch = map(int, version.split("."))
-
-# Increment patch version
-patch += 1
-data["version"] = f"{major}.{minor}.{patch}"
+# Assume version is an integer
+version = int(data.get("version", 0))
+version += 1
+data["version"] = version
 
 with open("firmware.json", "w") as f:
     json.dump(data, f, indent=2)
